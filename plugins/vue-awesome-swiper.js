65250ab15b3d805e090c1c0ee4f5a3b1cab3619a
dmytrokazak <dmytrokazak@gmail.com>
2022-05-13 01:50:38 +0300

    Swiper fix

--- plugins/vue-awesome-swiper.js
+++ plugins/vue-awesome-swiper.js
@@ -1,11 +1,5 @@
 import Vue from 'vue';
-import { Swiper as SwiperClass, Pagination, Mousewheel, Autoplay } from 'swiper/js/swiper.esm';
-import getAwesomeSwiper from 'vue-awesome-swiper/dist/exporter';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+import 'swiper/dist/css/swiper.css';
 
-// Swiper modules
-SwiperClass.use([Pagination, Mousewheel, Autoplay]);
-
-// -------------------------------------------------
-
-// Global use
-Vue.use(getAwesomeSwiper(SwiperClass));
+Vue.use(VueAwesomeSwiper);
